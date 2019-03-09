0623f6871785dc09c64fa57ad429e66954a217a3
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-09 03:44:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays(){
 
 function happyHolidaysTo(name) {
   name = "you"
-  return "Happy holidays, ${name}!"
+  return Happy holidays, ${name}!
 }
 
 function happyHolidayTo(holiday, name) {
