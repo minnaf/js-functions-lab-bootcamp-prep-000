251b486a12d1a565faa2fa9a6e52f69ab2f5117e
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-09 03:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name) {
-  name === "you"
+  var name = "you"
   return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo(holiday, name) {
-  holiday === "Independence Day"
-  name === "you"
+  var holiday = "Independence Day"
+  var name = "you"
   return "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown(holiday, days) {
-  holiday === "Mother's Day"
-  days === "20"
+  var holiday = "Mother's Day"
+  var days = "20"
   return "It's ${days} days until ${holiday}!"
 }
